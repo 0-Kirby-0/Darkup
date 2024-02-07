670174951976bdf6c38f23acd2a3d841ff3dd4f1
0-Kirby-0 <adlerkorbinian98@gmail.com>
2024-02-07 14:58:43 +0100

    Finalized and tested linebreak module

--- src/headers.rs
+++
@@ -1,3 +0,0 @@
-pub fn add(lines: &[&str]) -> Vec<String> {
-    vec![]
-}
--- src/textfixer.rs
+++ src/textfixer.rs
@@ -1,5 +1,4 @@
 use crate::defaults;
-use crate::headers;
 use crate::linebreaks;
 use crate::settings;
 
@@ -14,6 +13,48 @@ impl Default for Textfixer {
             settings: defaults::setting_list(),
         }
     }
+}
+
+impl Textfixer {
+    fn apply_linebreaks(&self, lines: &[String]) -> String {
+        let mut line_iter = lines.iter();
+        let Some(mut outstring) = line_iter.next().cloned() else {
+            return String::default(); //input was empty
+        };
+
+        for line in line_iter {
+            let previous_char = outstring
+                .chars()
+                .last()
+                .expect("Outstring was empty when getting previous char.");
+            let Some(following_char) = line.chars().next() else {
+                outstring += "\n"; //line is empty, add its linebreak and move on
+                continue;
+            };
+
+            if let Some(rule) = self
+                .ruleset
+                .iter()
+                .find(|r| r.matches(previous_char, following_char))
+            {
+                outstring = rule.merge(outstring, line); //apply matching rule
+            } else {
+                outstring = outstring + "\n" + &line; //no rule applies, add the linebreak and move on
+                continue;
+            };
+        }
+
+        outstring
+    }
+}
+
+fn get_count_item(s: &str) -> (u64, &str) {
+    let mut it = s.split(' ');
+    let (Some(count_str), Some(item)) = (it.next(), it.next()) else {
+        panic!("Can't segment count item pair: '{s}'");
+    };
+
+    (1, item)
 }
 
 /*TODO!
